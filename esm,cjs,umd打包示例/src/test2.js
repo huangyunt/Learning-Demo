83dcdf3311741de7d7077027f5cca9e99aaa623e
huangyunt <973113137@qq.com>
2025-05-30 23:36:16 +0800

    tradingView-demo

--- esm,cjs,umd打包示例/src/test2.js
+++ esm,cjs,umd打包示例/src/test2.js
@@ -1,3 +1,4 @@
-import { capitalize } from '../dist/string-utils.umd'
-
+// import _ from '../dist/string-utils.cjs'
+import { capitalize } from '../dist/string-utils.cjs.js'
+// const { capitalize } = require('../dist/string-utils.cjs.js')
 console.log('cap: ', capitalize('shaoiudoifa'))
